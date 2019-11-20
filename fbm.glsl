1e6db05e480848708efae1a3bf23d6a8364f5459
tuxalin <alin.loghin@gmail.com>
2019-11-20 13:54:11 +0200

    -cleanup of fbm

--- fbm.glsl
+++ fbm.glsl
@@ -1,13 +1,13 @@
 float fbmNoise(const in vec2 pos, const in vec2 scale, const in float angle) 
 {
     // based on classic value noise
-    vec2 i = floor(pos);
-    vec2 f = fract(pos);
+    vec2 p = mod(pos, scale);
+    vec2 i = floor(p);
+    vec2 f = fract(p);
 
     float cosAngle = cos(angle); 
     float sinAngle = sin(angle); 
-    mat2 rot = mat2(cosAngle, sinAngle,
-                   -sinAngle, cosAngle);
+    mat2 rot = mat2(cosAngle, sinAngle,-sinAngle, cosAngle);
     
     float a = hash(rot * i);
     float b = hash(rot * mod(i + vec2(1.0, 0.0), scale));
@@ -18,23 +18,21 @@ float fbmNoise(const in vec2 pos, const in vec2 scale, const in float angle)
     return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
 }
 
-float fbm(const in vec2 pos, const in vec2 scale, const int octaves, const float shift, const float axialShift) 
+float fbm(const in vec2 pos, const in vec2 scale, const int octaves, const float shift, const float axialShift, const in float lacunarity) 
 {
     // classic fbm implementation
     float value = 0.0;
     float amplitude = 0.5;
-    float frequency = 0.0;
     float angle = axialShift;
+    vec2 frequency = floor(scale);
     
-    vec2 s = floor(scale);
-    vec2 p = mod(pos * s, s);
-    for (int i = 0; i < 15; i++) 
+    vec2 p = mod(pos * frequency, frequency);
+    for (int i = 0; i < octaves; i++) 
     {
-        value += amplitude * fbmNoise(p, s, angle);
+        value += amplitude * fbmNoise(p, frequency, angle);
         
-        p = p * 2.0 + shift;
-        s *= 2.0;
-        p = mod(p, s);
+        p = p * lacunarity + shift;
+        frequency *= lacunarity;
         amplitude *= 0.5;
         angle += axialShift;
     }
