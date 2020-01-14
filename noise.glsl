a566c9937c278a02ca4156c21b808a77e4075754
Alin <tux_alin@yahoo.com>
2020-01-14 13:18:32 +0000

    -added gradient noise with derivative function

--- noise.glsl
+++ noise.glsl
@@ -99,10 +99,39 @@ float gradientNoise(const in vec2 pos, vec2 scale)
     return 2.0 * mix(ab, cd, u.y);
 }
 
-float perlinNoise(const in vec2 pos, const in vec2 scale)
+vec3 gradientNoised(const in vec2 pos, vec2 scale)
+{
+    // classic gradient noise with derivatives based on Inigo Quilez
+    scale = floor(scale);
+    vec2 p = mod(pos * scale, scale);
+    vec2 i = floor(p);
+    vec2 f = fract(p);
+    
+    vec2 a = hash2d(i);
+    vec2 b = hash2d(mod(i + vec2(1.0, 0.0), scale));
+    vec2 c = hash2d(mod(i + vec2(0.0, 1.0), scale));
+    vec2 d = hash2d(mod(i + vec2(1.0, 1.0), scale));
+
+    float va = dot(a, f);
+    float vb = dot(b, f - vec2(1.0, 0.0));
+    float vc = dot(c, f - vec2(0.0, 1.0));
+    float vd = dot(d, f - vec2(1.0, 1.0));
+
+    vec2 f2 = f * f;
+    vec2 u = f2 * f * (f * (f * 6.0 - 15.0) + 10.0); // = smootherstep
+    vec2 du = 30.0 * f2 * (f * (f - 2.0) + 1.0);
+
+    float value = va + u.x * (vb - va) + u.y * (vc - va) + u.x * u.y * (va - vb - vc + vd);
+    vec2 dxdy = a + u.x * (b-a) + u.y * (c - a) + u.x * u.y * (a - b - c + d);
+    dxdy += du * (u.yx * (va - vb - vc + vd) + vec2(vb, vc) - va);
+    return vec3(value * 0.5 + 0.5, dxdy);
+}
+
+float perlinNoise(const in vec2 pos, vec2 scale)
 {
     // classic Perlin noise based on Stefan Gustavson
-    vec2 p = pos * floor(scale);
+    scale = floor(scale);
+    vec2 p = pos * scale;
     vec4 i = mod(floor(p.xyxy) + vec4(0.0, 0.0, 1.0, 1.0), scale.xyxy);
     vec4 f = fract(p.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
     i = mod(i, 289.0); // avoid truncation effects in permutation
