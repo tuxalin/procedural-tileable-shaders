0bd98652632c42d662ec2911a8608c48e8f8f88e
tuxalin <alin.loghin@gmail.com>
2020-11-15 18:09:22 +0200

    -improved and updated the FBMs

--- fbm.glsl
+++ fbm.glsl
@@ -1,75 +1,187 @@
-
-float fbm(const in vec2 pos, const in vec2 scale, const int octaves, const float shift, const float axialShift, const in float gain, const in float lacunarity) 
+// Classic FBM implementation using Value noise.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param timeShift Time shift for each octave, range: [-inf, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param octaveFactor The octave intensity factor, the lower the more pronounced the lower octaves will be, range: [-1, 1], default: 0.0
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return value of the noise, range: [0, inf]
+float fbm(vec2 pos, vec2 scale, int octaves, float shift, float timeShift, float gain, float lacunarity, float octaveFactor, float seed) 
 {
-    // classic fbm implementation
-    float value = 0.0;
-    float amplitude = 0.5;
-    float angle = axialShift;
-    vec2 frequency = floor(scale);
-    
+    float amplitude = gain;
+    float time = timeShift;
+    vec2 frequency = scale;
+    vec2 offset = vec2(shift, 0.0);
     vec2 p = pos * frequency;
+    octaveFactor = 1.0 + octaveFactor * 0.12;
+    
+    vec2 sinCos = vec2(sin(shift), cos(shift));
+    mat2 rotate = mat2(sinCos.y, sinCos.x, sinCos.x, sinCos.y);
+
+    float value = 0.0;
     for (int i = 0; i < octaves; i++) 
     {
-        float cosAngle = cos(angle); 
-        float sinAngle = sin(angle); 
-        mat2 rot = mat2(cosAngle, sinAngle,-sinAngle, cosAngle);
-        value += amplitude * noise(p, frequency, rot);
+        float n = noise(p / frequency, frequency, time, seed);
+        value += amplitude * n;
         
-        p = p * lacunarity + shift;
+        p = p * lacunarity + offset * float(1 + i);
         frequency *= lacunarity;
-        amplitude *= gain;
-        angle += axialShift;
+        amplitude = pow(amplitude * gain, octaveFactor);
+        time += timeShift;
+        offset *= rotate;
     }
+    return value * 0.5 + 0.5;
+}
+
+// FBM implementation using Value noise with derivatives.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32], default: 2.0
+// @param slopeness Slope intensity of the derivatives, range: [0, 1], default: 0.5
+// @param octaveFactor The octave intensity factor, the lower the more pronounced the lower octaves will be, range: [-1, 1], default: 0.0
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return x = value of the noise, range: [0, inf], yz = derivative of the noise, range: [-1, 1]
+vec3 fbmd(vec2 pos, vec2 scale, int octaves, vec2 shift, float timeShift, float gain, vec2 lacunarity, float slopeness, float octaveFactor, float seed) 
+{
+    // fbm implementation based on Inigo Quilez
+    float amplitude = gain;
+    float time = timeShift;
+    vec2 frequency = scale;
+    vec2 p = pos * frequency;
+    octaveFactor = 1.0 + octaveFactor * 0.12;
+    
+    vec2 sinCos = vec2(sin(shift.x), cos(shift.y));
+    mat2 rotate = mat2(sinCos.y, sinCos.x, sinCos.x, sinCos.y);
+
+    vec3 value = vec3(0.0);
+    vec2 derivative = vec2(0.0);
+    for (int i = 0; i < octaves; i++) 
+    {
+        vec3 n =  noised(p / frequency, frequency, time, seed).xyz;
+        derivative += n.yz;
+
+        n *= amplitude;
+        n.x /= (1.0 + mix(0.0, dot(derivative, derivative), slopeness));
+        value += n; 
+        
+        p = (p + shift) * lacunarity;
+        frequency *= lacunarity;
+        amplitude = pow(amplitude * gain, octaveFactor);
+        shift = shift * rotate;
+        time += timeShift;
+    }
+    
+    value.x = value.x * 0.5 + 0.5;
     return value;
 }
-
-vec4 fbmVoronoi(const in vec2 pos, const in vec2 scale, const int octaves, const float shift, const float axialShift, const in float gain, const in float lacunarity, const in float jitter) 
+vec3 fbmd(vec2 pos, vec2 scale, int octaves, float shift, float timeShift, float gain, float lacunarity, float slopeness, float octaveFactor, float seed) 
 {
-    // classic fbm implementation with voronoi
-    vec3 value = vec3(0.0);
-    float lum = 0.0;
-    float amplitude = 0.5;
-    vec2 frequency = floor(scale);
-    float angle = axialShift;
-    
-    vec2 p = pos * frequency;
-    for (int i = 0; i < octaves; i++) 
-    {
-        float cosAngle = cos(angle); 
-        float sinAngle = sin(angle); 
-        mat2 rot = mat2(cosAngle, sinAngle,-sinAngle, cosAngle);
-        
-        vec2 tilePos;
-        value += amplitude * voronoi(p, frequency, jitter, rot, tilePos);
-        lum += amplitude * hash(tilePos);
-        
-        p = p * lacunarity + shift;
-        frequency *= lacunarity;
-        amplitude *= gain;
-        angle += axialShift;
-    }
-    return vec4(value, lum);
+    return fbmd(pos, scale, octaves, vec2(shift), timeShift, gain, vec2(lacunarity), slopeness, octaveFactor, seed);
+}
+vec3 fbmd(vec2 pos, vec2 scale, int octaves, vec2 shift, float timeShift, float gain, float lacunarity, float slopeness, float octaveFactor, float seed) 
+{
+    return fbmd(pos, scale, octaves, shift, timeShift, gain, vec2(lacunarity), slopeness, octaveFactor, seed);
 }
 
-vec3 fbmPerlin(const in vec2 pos, const in vec2 scale, const int octaves, const float shift, const float axialShift, const in float gain, const in float lacunarity, const in bool isMultiplyMode, const in float factor, const in float intensity) 
+// FBM implementation using Perlin noise with derivatives.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32], default: 2.0
+// @param slopeness Slope intensity of the derivatives, range: [0, 1], default: 0.5
+// @param octaveFactor The octave intensity factor, the lower the more pronounced the lower octaves will be, range: [-1, 1], default: 0.0
+// @param negative If true use a negative range for the noise values, will result in more contrast, range: [false, true]
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return x = value of the noise, range: [-1, inf], yz = derivative of the noise, range: [-1, 1]
+vec3 fbmdPerlin(vec2 pos, vec2 scale, int octaves, vec2 shift, mat2 transform, float gain, vec2 lacunarity, float slopeness, float octaveFactor, bool negative, float seed) 
 {
-    // classic fbm implementation with perlin
+    // fbm implementation based on Inigo Quilez
+    float amplitude = gain;
+    vec2 frequency = floor(scale);
+    vec2 p = pos * frequency;
+    octaveFactor = 1.0 + octaveFactor * 0.3;
+
     vec3 value = vec3(0.0);
-    float amplitude = 0.5;
+    vec2 derivative = vec2(0.0);
+    for (int i = 0; i < octaves; i++) 
+    {
+        vec3 n = perlinNoised(p / frequency, frequency, transform, seed);
+        derivative += n.yz;
+        n.x = negative ? n.x : n.x * 0.5 + 0.5;
+        n *= amplitude;
+        value.x += n.x / (1.0 + mix(0.0, dot(derivative, derivative), slopeness));
+        value.yz += n.yz; 
+        
+        p = (p + shift) * lacunarity;
+        frequency *= lacunarity;
+        amplitude = pow(amplitude * gain, octaveFactor);
+        transform *= transform;
+    }
+
+    return value;
+}
+vec3 fbmdPerlin(vec2 pos, vec2 scale, int octaves, vec2 shift, float axialShift, float gain, vec2 lacunarity, float slopeness, float octaveFactor, bool negative, float seed) 
+{
+    vec2 cosSin = vec2(cos(axialShift), sin(axialShift));
+    mat2 transform = mat2(cosSin.x, cosSin.y, -cosSin.y, cosSin.x) * mat2(0.8, -0.6, 0.6, 0.8);
+    return fbmdPerlin(pos, scale, octaves, shift, transform, gain, lacunarity, slopeness, octaveFactor, negative, seed);
+}
+
+// FBM implementation using Perlin noise, can also be used to create ridges based on the mode used.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param axialShift Axial or rotational shift for each octave, range: [-inf, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param mode Mode used in combining the noise for the ocatves, range: [0, 5]
+// @param factor Pow intensity factor, range: [0, 10], default: 1.0
+// @param offset Offsets the value of the noise, range: [-1, 1], default: 0.0
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return value of the noise, range: [0, inf]
+float fbmPerlin(vec2 pos, vec2 scale, int octaves, float shift, float axialShift, float gain, float lacunarity, uint mode, float factor, float offset, float seed) 
+{
+    float amplitude = gain;
     vec2 frequency = floor(scale);
     float angle = axialShift;
     float n = 1.0;
-    
-    vec2 p = pos * frequency;
+    vec2 p = fract(pos) * frequency;
+
+    float value = 0.0;
     for (int i = 0; i < octaves; i++) 
     {
-        float cosAngle = cos(angle); 
-        float sinAngle = sin(angle); 
-        mat2 rot = mat2(cosAngle, sinAngle,-sinAngle, cosAngle);
+        float pn = perlinNoise(p / frequency, frequency, angle) + offset;
+        if (mode == 0u)
+        {
+            n *= abs(pn);
+        }
+        else if (mode == 1u)
+        {
+            n = abs(pn);
+        }
+        else if (mode == 2u)
+        {
+            n = pn;
+        }
+        else if (mode == 3u)
+        {
+            n *= pn;
+        }
+        else if (mode == 4u)
+        {
+            n = pn * 0.5 + 0.5;
+        }
+        else
+        {
+            n *= pn * 0.5 + 0.5;
+        }
         
-        float pn = abs(perlinNoise(p, frequency, rot));
-        n = isMultiplyMode ? n * pn : pn;
-        n = pow(n, factor) * intensity;
+        n = pow(n < 0.0 ? 0.0 : n, factor);
         value += amplitude * n;
         
         p = p * lacunarity + shift;
@@ -80,27 +192,142 @@ vec3 fbmPerlin(const in vec2 pos, const in vec2 scale, const int octaves, const 
     return value;
 }
 
-float fbmd(const in vec2 pos, const in vec2 scale, const int octaves, const float shift, const in float gain, const in float lacunarity, const in float intensity) 
+// FBM implementation using Voronoi.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param timeShift Time shift for each octave, range: [-inf, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param octaveFactor The octave intensity factor, the lower the more pronounced the lower octaves will be, range: [-1, 1], default: 0.0
+// @param jitter Jitter factor for the cells, if zero then it will result in a square grid, range: [0, 1], default: 1.0
+// @param interpolate Interpolate factor between the multiplication mode and normal mode, default: 0.0
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return value of the noise, range: [0, inf]
+vec4 fbmVoronoi(vec2 pos, vec2 scale, int octaves, float shift, float timeShift, float gain, float lacunarity, float octaveFactor, float jitter, float interpolate, float seed) 
 {
-    // fbm implementation based on Inigo Quilez
-    float value = 0.0;
-    vec2 derivative = vec2(0.0);
-    float amplitude = 0.5;
-    vec2 frequency = floor(scale);
-    
-    mat2 rot = mat2(0.8,-0.6,0.6,0.8);
+    float amplitude = gain;
+    float time = timeShift;
+    vec2 frequency = scale;
+    vec2 offset = vec2(shift, 0.0);
     vec2 p = pos * frequency;
+    octaveFactor = 1.0 + octaveFactor * 0.12;
+    
+    vec2 sinCos = vec2(sin(shift), cos(shift));
+    mat2 rotate = mat2(sinCos.y, sinCos.x, sinCos.x, sinCos.y);
+    
+    float n = 1.0;
+    vec4 value = vec4(0.0);
     for (int i = 0; i < octaves; i++) 
     {
-        vec3 n = noised(p, frequency, rot);
-        derivative += n.yz;
-        value += amplitude * n.x / (1.0 + mix(0.0, dot(derivative, derivative), intensity));
+        vec3 v = voronoi(p / frequency, frequency, jitter, timeShift, seed);
+        v.x = v.x * 2.0 - 1.0;
+        n *= v.x;
+        value += amplitude * vec4(mix(v.x, n, interpolate), hash3D(v.yz));
         
-        p = p * lacunarity + shift;
+        p = p * lacunarity + offset * float(1 + i);
         frequency *= lacunarity;
-        amplitude *= gain;
-        
-        rot *= rot;
+        amplitude = pow(amplitude * gain, octaveFactor);
+        time += timeShift;
+        offset *= rotate;
     }
+    value.x = value.x * 0.5 + 0.5;
     return value;
+}
+
+// FBM implementation using a variation of Value noise.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param timeShift Time shift for each octave, range: [-inf, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param translate Translate factors for the value noise , range: [-inf, inf], default: {0.5, -0.25, 0.15}
+// @param warpStrength The warp factor used for domain warping, range: [-10, 10], default: 0.5
+// @param octaveFactor The octave intensity factor, the lower the more pronounced the lower octaves will be, range: [-1, 1], default: 0.0
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return value of the noise, range: [0, inf]
+float fbmGrid(vec2 pos, vec2 scale, int octaves, float shift, float timeShift, float gain, float lacunarity, vec3 translate, float warpStrength, float octaveFactor, float seed) 
+{
+    float amplitude = gain;
+    float time = timeShift;
+    vec2 frequency = scale;
+    vec2 offset = vec2(shift, 0.0);
+    vec2 p = pos * frequency;
+    octaveFactor = 1.0 + octaveFactor * 0.12;
+    
+    vec2 sinCos = vec2(sin(shift), cos(shift));
+    mat2 rotate = mat2(sinCos.y, sinCos.x, sinCos.x, sinCos.y);
+
+    float value = 0.0;
+    for (int i = 0; i < octaves; i++) 
+    {
+        // TODO: optimize via multi return value noise
+        vec2 pi = p / frequency + value * warpStrength;
+        vec4 mn;;
+        mn.x = noise(pi, frequency, time, seed);
+        mn.y = noise(pi + translate.x, frequency, time, seed);
+        mn.z = noise(pi + translate.y, frequency, time, seed);
+        mn.w = noise(pi + translate.z, frequency, time, seed);
+        mn.xy = mn.xy * mn.zw;
+
+        float n = pow(abs(mn.x * mn.y), 0.25) * 2.0 - 1.0;
+        value += amplitude * n;
+        
+        p = p * lacunarity + offset * float(1 + i);
+        frequency *= lacunarity;
+        amplitude = pow(amplitude * gain, octaveFactor);
+        time += timeShift;
+        offset *= rotate;
+    }
+    value = value * 0.5 + 0.5;
+    return value * value;
+}
+
+float fbmGrid(vec2 pos, vec2 scale, int octaves, float shift, float timeShift, float gain, float lacunarity, float warpStrength, float octaveFactor, float seed) 
+{
+    vec3 translate = (hash3D(vec2(seed)) * 2.0 - 1.0) * scale.xyx;
+    return fbmGrid(pos, scale, octaves, shift, timeShift, gain, lacunarity, translate, warpStrength, octaveFactor, seed);
+}
+
+// FBM implementation using metaballs.
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param timeShift Time shift for each octave, range: [-inf, inf]
+// @param gain Gain for each fbm octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param octaveFactor The octave intensity factor, the lower the more pronounced the lower octaves will be, range: [-1, 1], default: 0.0
+// @param interpolate Interpolate factor between the multiplication mode and normal mode, default: 0.0
+// @param jitter Jitter factor for the cells, if zero then it will result in a square grid, range: [0, 1], default: 1.0
+// @param width Width and softness of the metaballs, range: [0, 1], default: {0.1, 0.01}
+// @param seed Seed to randomize result, range: [0, inf], default: 0.0
+// @return value of the noise, range: [0, inf]
+float fbmMetaballs(vec2 pos, vec2 scale, int octaves, float shift, float timeShift, float gain, float lacunarity, float octaveFactor, float jitter, float interpolate, vec2 width, float seed) 
+{
+    float amplitude = gain;
+    float time = timeShift;
+    vec2 frequency = scale;
+    vec2 offset = vec2(shift, 0.0);
+    vec2 p = pos * frequency;
+    octaveFactor = 1.0 + octaveFactor * 0.12;
+    
+    vec2 sinCos = vec2(sin(shift), cos(shift));
+    mat2 rotate = mat2(sinCos.y, sinCos.x, sinCos.x, sinCos.y);
+    
+    float n = 1.0;
+    float value = 0.0;
+    for (int i = 0; i < octaves; i++) 
+    {
+        float cn = metaballs(p / frequency, frequency, jitter, timeShift, width.x, width.y, seed) * 2.0 - 1.0;
+        n *= cn;
+        value += amplitude * mix(n, abs(n), interpolate);
+        
+        p = p * lacunarity + offset * float(1 + i);
+        frequency *= lacunarity;
+        amplitude = pow(amplitude * gain, octaveFactor);
+        time += timeShift;
+        offset *= rotate;
+    }
+    return value * 0.5 + 0.5;
 }
--- fbmImage.glsl
+++ fbmImage.glsl
@@ -21,32 +21,39 @@ vec2 grayscaleSobel(sampler2D tex, vec2 uv, float spread)
 {
     vec3 offset = vec3(1.0 / vec2(textureSize(tex, 0)), 0.0) * spread;
     vec2 grad = vec2(0.0);
-    grad.x -= texture(tex, uv - offset.xy) * 1.0;
-    grad.x -= texture(tex, uv - offset.xz) * 2.0;
-    grad.x -= texture(tex, uv + offset.xy * vec2(-1.0, 1.0)) * 1.0;
-    grad.x += texture(tex, uv + offset.xy * vec2(1.0, -1.0)) * 1.0;
-    grad.x += texture(tex, uv + offset.xz) * 2.0;
-    grad.x += texture(tex, uv + offset.xy) * 1.0;
-    grad.y -= texture(tex, uv - offset.xy) * 1.0;
-    grad.y -= texture(tex, uv - offset.zy) * 2.0;
-    grad.y -= texture(tex, uv + offset.xy * vec2(1.0, -1.0)) * 1.0;
-    grad.y += texture(tex, uv + offset.xy * vec2(-1.0, 1.0)) * 1.0;
-    grad.y += texture(tex, uv + offset.zy) * 2.0;
-    grad.y += texture(tex, uv + offset.xy) * 1.0;
+    grad.x -= texture(tex, uv - offset.xy).r * 1.0;
+    grad.x -= texture(tex, uv - offset.xz).r * 2.0;
+    grad.x -= texture(tex, uv + offset.xy * vec2(-1.0, 1.0)).r * 1.0;
+    grad.x += texture(tex, uv + offset.xy * vec2(1.0, -1.0)).r * 1.0;
+    grad.x += texture(tex, uv + offset.xz).r * 2.0;
+    grad.x += texture(tex, uv + offset.xy).r * 1.0;
+    grad.y -= texture(tex, uv - offset.xy).r * 1.0;
+    grad.y -= texture(tex, uv - offset.zy).r * 2.0;
+    grad.y -= texture(tex, uv + offset.xy * vec2(1.0, -1.0)).r * 1.0;
+    grad.y += texture(tex, uv + offset.xy * vec2(-1.0, 1.0)).r * 1.0;
+    grad.y += texture(tex, uv + offset.zy).r * 2.0;
+    grad.y += texture(tex, uv + offset.xy).r * 1.0;
     return grad;
 }
 
-// based on derivative fbm by Inigo Quilez
-vec3 fbmImage(sampler2D tex, vec2 uv, vec2 scale, uint octaves, float shift, float axialShift, float gain, float intensity, float spread, uint lacunarity)
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param axialShift Axial or rotational shift for each octave, range: [0, inf]
+// @param gain Gain for each octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param slopeness Slope intensity of the derivatives, range: [0, 1], default: 0.5
+// @param spread Spread for the derivatives, range: [1, 16], default: 1.0
+vec3 fbmImage(sampler2D tex, vec2 uv, vec2 scale, uint octaves, float shift, float axialShift, float gain, float lacunarity, float slopeness, float spread)
 {
+    // based on derivative fbm by Inigo Quilez
     vec3 value = vec3(0.0);
     vec2 derivative = vec2(0.0);
     
-    float amplitude = 0.5;
+    float amplitude = gain;
     vec2 frequency = floor(scale);
     float angle = 0.0;
     axialShift =  3.1415926 * 0.5 * floor(float(octaves) * axialShift);
-    float lacunarityf = float(lacunarity);
 
     vec2 p = uv * frequency;
     for (uint i = 0u; i < octaves; i++)
@@ -58,26 +65,33 @@ vec3 fbmImage(sampler2D tex, vec2 uv, vec2 scale, uint octaves, float shift, flo
         vec3 color = texture(tex, coord).rgb;
         vec2 grad = sobel(tex, coord, spread);
         derivative += grad;
-        value += amplitude * color / (1.0 + mix(0.0, dot(derivative, derivative), intensity));
+        value += amplitude * color / (1.0 + mix(0.0, dot(derivative, derivative), slopeness));
 
         amplitude *= gain;
-        p = p * lacunarityf + shift;
-        frequency *= lacunarityf;
+        p = p * lacunarity + shift;
+        frequency *= lacunarity;
         angle += axialShift;
     }
     return value;
 }
 
-vec3 fbmGrayscaleImaged(sampler2D tex, vec2 uv, vec2 scale, uint octaves, float shift, float axialShift, float gain, float intensity, float spread, uint lacunarity)
+// @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
+// @param octaves Number of octaves for the fbm, range: [1, inf]
+// @param shift Position shift for each octave, range: [0, inf]
+// @param axialShift Axial or rotational shift for each octave, range: [0, inf]
+// @param gain Gain for each octave, range: [0, 2], default: 0.5
+// @param lacunarity Frequency of the fbm, must be integer for tileable results, range: [1, 32]
+// @param slopeness Slope intensity of the derivatives, range: [0, 1], default: 0.5
+// @param spread Spread for the derivatives, range: [1, 16], default: 1.0
+vec3 fbmGrayscaleImaged(sampler2D tex, vec2 uv, vec2 scale, uint octaves, float shift, float axialShift, float gain, float lacunarity, float slopeness, float spread)
 {
     float value = 0.0;
     vec2 derivative = vec2(0.0);
     
-    float amplitude = 0.5;
+    float amplitude = gain;
     vec2 frequency = floor(scale);
     float angle = 0.0;
     axialShift =  3.1415926 * 0.5 * floor(float(octaves) * axialShift);
-    float lacunarityf = float(lacunarity);
 
     vec2 p = uv * frequency;
     for (uint i = 0u; i < octaves; i++)
@@ -89,11 +103,11 @@ vec3 fbmGrayscaleImaged(sampler2D tex, vec2 uv, vec2 scale, uint octaves, float 
         float lum = texture(tex, coord).r;
         vec2 grad = grayscaleSobel(tex, coord, spread);
         derivative += grad;
-        value += amplitude * lum / (1.0 + mix(0.0, dot(derivative, derivative), intensity));
+        value += amplitude * lum / (1.0 + mix(0.0, dot(derivative, derivative), slopeness));
 
         amplitude *= gain;
-        p = p * lacunarityf + shift;
-        frequency *= lacunarityf;
+        p = p * lacunarity + shift;
+        frequency *= lacunarity;
         angle += axialShift;
     }
     return vec3(value, derivative);
