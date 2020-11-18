2b2a6285fb7e2977e2786cb5ad9148101ff373ac
tuxalin <alin.loghin@gmail.com>
2020-11-18 17:47:54 +0200

    -optimized the gradient noise

--- gradientNoise.glsl
+++ gradientNoise.glsl
@@ -8,18 +8,13 @@ float gradientNoise(vec2 pos, vec2 scale, float seed)
     vec4 f = (pos.xyxy - i.xyxy) - vec2(0.0, 1.0).xxyy;
     i = mod(i, scale.xyxy) + seed;
 
-    vec4 hash0, hash1;
-    smultiHash2D(i, hash0, hash1);
-    vec2 a = vec2(hash0.x, hash1.x);
-    vec2 b = vec2(hash0.y, hash1.y);
-    vec2 c = vec2(hash0.z, hash1.z);
-    vec2 d = vec2(hash0.w, hash1.w);
-    
+    vec4 hashX, hashY;
+    smultiHash2D(i, hashX, hashY);
+
+    vec4 gradients = hashX * f.xzxz + hashY * f.yyww;
     vec2 u = noiseInterpolate(f.xy);
-    vec2 g0 = vec2(dot(a, f.xy), dot(c, f.xw));
-    vec2 g1 = vec2(dot(b, f.zy), dot(d, f.zw));
-    vec2 g = mix(g0, g1, u.x);
-    return 1.7 * mix(g.x, g.y, u.y);
+    vec2 g = mix(gradients.xz, gradients.yw, u.x);
+    return 1.4142135623730950 * mix(g.x, g.y, u.y);
 }
 
 // 2D Gradient noise with gradients transform (i.e. can be used to rotate the gradients).
@@ -33,18 +28,32 @@ float gradientNoise(vec2 pos, vec2 scale, mat2 transform, float seed)
     vec4 f = (pos.xyxy - i.xyxy) - vec2(0.0, 1.0).xxyy;
     i = mod(i, scale.xyxy) + seed;
     
-    vec4 hash0, hash1;
-    smultiHash2D(i, hash0, hash1);
-    vec2 a = transform * vec2(hash0.x, hash1.x);
-    vec2 b = transform * vec2(hash0.y, hash1.y);
-    vec2 c = transform * vec2(hash0.z, hash1.z);
-    vec2 d = transform * vec2(hash0.w, hash1.w);
+    vec4 hashX, hashY;
+    smultiHash2D(i, hashX, hashY);
+
+    // transform gradients
+    vec4 rhash = vec4(hashX.x, hashY.x, hashX.y, hashY.y);
+    rhash.xy = transform * rhash.xy;
+    rhash.zw = transform * rhash.zw;
+    hashX.xy = rhash.xz;
+    hashY.xy = rhash.yw;
+
+    rhash = vec4(hashX.z, hashY.z, hashX.w, hashY.w);
+    rhash.xy = transform * rhash.xy;
+    rhash.zw = transform * rhash.zw;
+    hashX.zw = rhash.xz;
+    hashY.zw = rhash.yw;
     
+    vec4 gradients = hashX * f.xzxz + hashY * f.yyww;
     vec2 u = noiseInterpolate(f.xy);
-    vec2 g0 = vec2(dot(a, f.xy), dot(c, f.xw));
-    vec2 g1 = vec2(dot(b, f.zy), dot(d, f.zw));
-    vec2 g = mix(g0, g1, u.x);
-    return 1.7 * mix(g.x, g.y, u.y);
+    vec2 g = mix(gradients.xz, gradients.yw, u.x);
+    return 1.4142135623730950 * mix(g.x, g.y, u.y);
+}
+
+float gradientNoise(vec2 pos, vec2 scale, float rotation, float seed) 
+{
+    vec2 sinCos = vec2(sin(rotation), cos(rotation));
+    return gradientNoise(pos, scale, mat2(sinCos.y, sinCos.x, sinCos.x, sinCos.y), seed);
 }
 
 // 2D Gradient noise with derivatives.
@@ -58,25 +67,22 @@ vec3 gradientNoised(vec2 pos, vec2 scale, float seed)
     vec4 f = (pos.xyxy - i.xyxy) - vec2(0.0, 1.0).xxyy;
     i = mod(i, scale.xyxy) + seed;
     
-    vec4 hash0, hash1;
-    smultiHash2D(i, hash0, hash1);
-    vec2 a = vec2(hash0.x, hash1.x);
-    vec2 b = vec2(hash0.y, hash1.y);
-    vec2 c = vec2(hash0.z, hash1.z);
-    vec2 d = vec2(hash0.w, hash1.w);
-
-    float va = dot(a, f.xy);
-    float vb = dot(b, f.zy);
-    float vc = dot(c, f.xw);
-    float vd = dot(d, f.zw);
+    vec4 hashX, hashY;
+    smultiHash2D(i, hashX, hashY);
+    vec2 a = vec2(hashX.x, hashY.x);
+    vec2 b = vec2(hashX.y, hashY.y);
+    vec2 c = vec2(hashX.z, hashY.z);
+    vec2 d = vec2(hashX.w, hashY.w);
+    
+    vec4 gradients = hashX * f.xzxz + hashY * f.yyww;
 
     vec4 udu = noiseInterpolateDu(f.xy);
     vec2 u = udu.xy;
-
-    float value = va + u.x * (vb - va) + u.y * (vc - va) + u.x * u.y * (va - vb - vc + vd);
-    vec2 dxdy = a + u.x * (b-a) + u.y * (c - a) + u.x * u.y * (a - b - c + d);
-    dxdy += udu.zw * (u.yx * (va - vb - vc + vd) + vec2(vb, vc) - va);
-    return vec3(value * 1.7, dxdy);
+    vec2 g = mix(gradients.xz, gradients.yw, u.x);
+    
+    vec2 dxdy = a + u.x * (b - a) + u.y * (c - a) + u.x * u.y * (a - b - c + d);
+    dxdy += udu.zw * (u.yx * (gradients.x - gradients.y - gradients.z + gradients.w) + gradients.yz - gradients.x);
+    return vec3(mix(g.x, g.y, u.y) * 1.4142135623730950, dxdy);
 }
 
 // 2D Gradient noise with gradients transform (i.e. can be used to rotate the gradients) and derivatives.
@@ -91,25 +97,36 @@ vec3 gradientNoised(vec2 pos, vec2 scale, mat2 transform, float seed)
     vec4 f = (pos.xyxy - i.xyxy) - vec2(0.0, 1.0).xxyy;
     i = mod(i, scale.xyxy) + seed;
     
-    vec4 hash0, hash1;
-    smultiHash2D(i, hash0, hash1);
-    vec2 a = transform * vec2(hash0.x, hash1.x);
-    vec2 b = transform * vec2(hash0.y, hash1.y);
-    vec2 c = transform * vec2(hash0.z, hash1.z);
-    vec2 d = transform * vec2(hash0.w, hash1.w);
+    vec4 hashX, hashY;
+    smultiHash2D(i, hashX, hashY);
 
-    float va = dot(a, f.xy);
-    float vb = dot(b, f.zy);
-    float vc = dot(c, f.xw);
-    float vd = dot(d, f.zw);
+    // transform gradients
+    vec4 rhash = vec4(hashX.x, hashY.x, hashX.y, hashY.y);
+    rhash.xy = transform * rhash.xy;
+    rhash.zw = transform * rhash.zw;
+    hashX.xy = rhash.xz;
+    hashY.xy = rhash.yw;
+
+    rhash = vec4(hashX.z, hashY.z, hashX.w, hashY.w);
+    rhash.xy = transform * rhash.xy;
+    rhash.zw = transform * rhash.zw;
+    hashX.zw = rhash.xz;
+    hashY.zw = rhash.yw;
+    
+    vec2 a = vec2(hashX.x, hashY.x);
+    vec2 b = vec2(hashX.y, hashY.y);
+    vec2 c = vec2(hashX.z, hashY.z);
+    vec2 d = vec2(hashX.w, hashY.w);
+    
+    vec4 gradients = hashX * f.xzxz + hashY * f.yyww;
 
     vec4 udu = noiseInterpolateDu(f.xy);
     vec2 u = udu.xy;
-
-    float value = va + u.x * (vb - va) + u.y * (vc - va) + u.x * u.y * (va - vb - vc + vd);
-    vec2 dxdy = a + u.x * (b-a) + u.y * (c - a) + u.x * u.y * (a - b - c + d);
-    dxdy += udu.zw * (u.yx * (va - vb - vc + vd) + vec2(vb, vc) - va);
-    return vec3(value * 1.7, dxdy);
+    vec2 g = mix(gradients.xz, gradients.yw, u.x);
+    
+    vec2 dxdy = a + u.x * (b - a) + u.y * (c - a) + u.x * u.y * (a - b - c + d);
+    dxdy += udu.zw * (u.yx * (gradients.x - gradients.y - gradients.z + gradients.w) + gradients.yz - gradients.x);
+    return vec3(mix(g.x, g.y, u.y) * 1.4142135623730950, dxdy);
 }
 
 // @param scale Number of tiles, must be  integer for tileable results, range: [2, inf]
